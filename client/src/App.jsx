fdb4ab4fac1f749375f4f14db63dc39a315aeba3
Yugal <yugalramesh6832@gmail.com>
2024-12-11 11:30:57 +0530

    create header comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Profile from "./pages/Profile";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
