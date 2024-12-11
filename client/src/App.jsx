6666612f3cde98bbec5fc690f889defd5a1ee0dd
Yugal <yugalramesh6832@gmail.com>
2024-12-11 10:49:22 +0530

    install react & tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="underline text-blue-200">hello</h1>
+      <h1 className="underline text-blue-500">hee</h1>
+    </div>
+  );
+}        
+
+export default App;
