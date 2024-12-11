20b5faea4066278e295d83ecf0ad82d47d7b0a85
Yugal <yugalramesh6832@gmail.com>
2024-12-11 12:00:02 +0530

    create and run server

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors'
+
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+// mongoose.connect(process.env.MONGO)
+// .then(() => console.log(`MongoDB Connect !!!`))
+// .catch(() => console.log(`Error While Connect MongoDB !!!`))
+
+const port =  8954;
+
+app.listen(port, () => {
+    console.log(`Server Run On Port : ${port}`);
+    
+})
